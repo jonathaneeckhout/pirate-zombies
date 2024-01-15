4c3b45b153d9cf93a35af4bf6fbe2e350ac14c0f
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-01-15 20:43:38 +0100

    add documentation

--- components/PlayerClientAuthorityController/PlayerClientAuthorityController.gd
+++ components/PlayerClientAuthorityController/PlayerClientAuthorityController.gd
@@ -1,74 +1,77 @@
+# This script extends Node3D and represents a client-side player controller with authority.
 extends Node3D
 
-## This component takes care of the controls of the player. This component will only take affect on the player's own character.
+## Define the class name for the script.
 class_name PlayerClientAuthorityController
 
+## The component name for registration in the multiplayer connection's component list.
 const COMPONENT_NAME = "PlayerClientAuthorityController"
 
-## The gravity that will be used for the player
+## Gravity used for the player.
 @export var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-## The movement speed of the player
+## Movement speed of the player.
 @export var movement_speed: float = 10.0
 
-## the jump speed of the player
+## Jump speed of the player.
 @export var jump_speed: float = 5.0
 
-## The mouse sensivity of the player
-@export var mouse_sensivity: float = 0.4
+## Mouse sensitivity of the player.
+@export var mouse_sensitivity: float = 0.4
 
-## Key being used to jump
+## Key for jumping.
 @export var jump_key: String = "jump"
 
-## Key being used to move up
+## Key for moving up.
 @export var up_key: String = "up"
 
-## Key being used to move down
+## Key for moving down.
 @export var down_key: String = "down"
 
-## Key being used to move left
+## Key for moving left.
 @export var left_key: String = "left"
 
-## Key being used to jump
+## Key for moving right.
 @export var right_key: String = "right"
 
-# This serves as the parent node on which the component will take effect.
+# Reference to the parent node (assumed to be a Player node).
 var _player: Player = null
 
+# Reference to the ClockSynchronizer component for timestamp synchronization.
 var _clock_synchronizer: ClockSynchronizer = null
 
-# The timestamp of the last received sync message from the server
+# The timestamp of the last received sync message from the server.
 var _last_sync_timestamp: float = 0.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Get the player, make sure this component is a child of the player's object
+	# Get the player; make sure this component is a child of the player's object.
 	_player = get_parent()
 
 	assert(_player.multiplayer_connection != null, "Player's multiplayer connection is null")
 
-	# Register yourself with your parent
+	# Register the component with the parent player.
 	_player.multiplayer_connection.component_list.register_component(COMPONENT_NAME, self)
 
-	_clock_synchronizer = (_player.multiplayer_connection.component_list.get_component(
+	# Get the ClockSynchronizer component.
+	_clock_synchronizer = _player.multiplayer_connection.component_list.get_component(
 		ClockSynchronizer.COMPONENT_NAME
-	))
+	)
 
-	assert(_clock_synchronizer != null, "Failed to get PlayerSpawnerSynchronizer component")
+	assert(_clock_synchronizer != null, "Failed to get ClockSynchronizer component")
 
-	# Server-side code
+	# Server-side code.
 	if _player.multiplayer_connection.is_server():
-		# Don't handle input on server-side
+		# Don't handle input or run physics process on the server-side.
 		set_process_input(false)
-
-		# No need to run the physics process
 		set_physics_process(false)
 		return
 
-	# Client-side code
+	# Client-side code.
 	else:
-		# This component is only needed for your own player on the client-side, thus it can be deleted for the other players
+		# This component is only needed for the client's own player.
+		# Delete it for other players.
 		if not _player.multiplayer_connection.is_own_player(_player):
 			set_process_input(false)
 			set_physics_process(false)
@@ -76,49 +79,52 @@ func _ready():
 			return
 
 
+# Handles mouse motion input to rotate the player and look up and down.
 func _input(event):
 	if event is InputEventMouseMotion:
-		# Rotate the player around your axis
-		_player.rotate_y(deg_to_rad(-event.relative.x * mouse_sensivity))
+		# Rotate the player around the axis.
+		_player.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
 
-		# Look up and down
-		_player.head.rotate_x(deg_to_rad(-event.relative.y * mouse_sensivity))
+		# Look up and down.
+		_player.head.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
 
-		# Make sure to not look too far
+		# Ensure not to look too far.
 		_player.head.rotation.x = clamp(_player.head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
 
+# Handles physics processing to apply gravity, jump, and movement.
 func _physics_process(delta):
-	# Apply gravity of the player is not on the floor (airtime)
+	# Apply gravity if the player is not on the floor (in air).
 	if not _player.is_on_floor():
 		_player.velocity.y -= gravity * delta
 
-	# Only allow the player to jump when on the floor
+	# Allow the player to jump only when on the floor.
 	if Input.is_action_just_pressed(jump_key) and _player.is_on_floor():
 		_player.velocity.y = jump_speed
 
-	# Only allow the movement of direction when on the ground
+	# Allow movement only when on the ground.
 	if _player.is_on_floor():
-		# Get the input direction from the player's input
+		# Get the input direction from the player's input.
 		var input_dir = Input.get_vector(left_key, right_key, up_key, down_key)
 
-		# Calculate the direction compared to the current player's transform basis
+		# Calculate the direction compared to the current player's transform basis.
 		var direction = (
 			(_player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		)
 
-		# If input is detected, apply it to the velocity
+		# If input is detected, apply it to the velocity.
 		if direction:
 			_player.velocity.x = direction.x * movement_speed
 			_player.velocity.z = direction.z * movement_speed
-		# If not slow down
+		# If not, slow down.
 		else:
 			_player.velocity.x = move_toward(_player.velocity.x, 0, movement_speed) * delta
 			_player.velocity.z = move_toward(_player.velocity.z, 0, movement_speed) * delta
 
+	# Move and slide the player.
 	_player.move_and_slide()
 
-	# Sync your position to the server
+	# Sync the position to the server.
 	_sync_position.rpc_id(
 		1,
 		_clock_synchronizer.client_clock,
@@ -128,20 +134,22 @@ func _physics_process(delta):
 	)
 
 
-## This function stores the latest received sync information for this entity. This information is later used to smoothly inter or extrapolate the position of the entity.
+# Stores the latest received sync information for this entity.
+# This information is later used to smoothly interpolate or extrapolate the position of the entity.
 func _server_sync_position(timestamp: float, pos: Vector3, rot: float, head: float):
-	# Ignore older syncs
+	# Ignore older syncs.
 	if timestamp < _last_sync_timestamp:
 		return
 
 	_last_sync_timestamp = timestamp
 
-	# Update the player's values
+	# Update the player's values.
 	_player.position = pos
 	_player.rotation.y = rot
 	_player.head.rotation.x = head
 
 
+# RPC function to sync the position from the client to the server.
 @rpc("call_remote", "any_peer", "unreliable")
 func _sync_position(t: float, p: Vector3, r: float, h: float):
 	assert(_player.multiplayer_connection.is_server(), "This call can only run on the server")
