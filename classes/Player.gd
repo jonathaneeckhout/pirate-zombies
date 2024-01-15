20532e83c9e8afec03d4be6ac8910022fed063bb
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-01-15 20:36:51 +0100

    sync player movement over network

--- classes/Player.gd
+++ classes/Player.gd
@@ -1,27 +1,33 @@
+# This script extends CharacterBody3D and represents a player in the multiplayer game.
 extends CharacterBody3D
 
+# Define the class name for the script.
 class_name Player
 
-## The component used for the multiplayer functionality
+# Reference to the multiplayer connection component.
 var multiplayer_connection: MultiplayerConnection = null
 
-## The id of the multiplayer connection
+# The ID of the multiplayer connection.
 var peer_id: int = 0
 
+# Reference to the camera and head nodes.
 @onready var camera: Camera3D = %Camera3D
 @onready var head: Node3D = %Head
 
-
+# Called when the node is added to the scene for the first time.
 func _ready():
-	# Add the player to the players group
+	# Add the player to the "players" group.
 	add_to_group("players")
 
+	# Check if the player is on the server side.
 	if multiplayer_connection.is_server():
-		# Don't handle input on the server side
+		# Don't handle input on the server side.
 		set_process_input(false)
 	else:
+		# Check if the player is the local client's own player instance.
 		if multiplayer_connection.is_own_player(self):
-			# Use this camera on the client side
+			# Use this camera on the client side.
 			camera.current = true
 		else:
+			# Disable input processing for other clients' player instances.
 			set_process_input(false)
--- components/MouseGrabber/MouseGrabber.gd
+++ components/MouseGrabber/MouseGrabber.gd
@@ -1,29 +1,37 @@
+# This script extends Node3D, serving as a component to control mouse input.
 extends Node3D
 
+# Exported variable to set the key for grabbing and releasing the mouse.
 @export var grab_mouse_key: String = "grab_mouse"
 
-# This serves as the parent node on which the component will take effect.
+# Reference to the parent node (assumed to be a Player node).
 var _player: Player = null
 
-
+# Called when the node is added to the scene for the first time.
 func _ready():
+	# Get the parent node (assumed to be a Player node).
 	_player = get_parent()
 
+	# Ensure that the player's multiplayer connection is not null.
 	assert(_player.multiplayer_connection != null, "Player's multiplayer connection is null")
 
+	# Check if the player is a client and owns its player instance.
 	if (
 		not _player.multiplayer_connection.is_server()
 		and _player.multiplayer_connection.is_own_player(_player)
 	):
-		# Disable the mouse from the view
+		# Disable the mouse from the view by capturing it.
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	else:
+		# If not a client or not owning the player instance, disable input processing and free the node.
 		set_process_input(false)
 		queue_free()
 
-
+# Called every frame. Handles mouse input events.
 func _input(event):
+	# Check if the grab_mouse_key is pressed.
 	if event.is_action_pressed(grab_mouse_key):
+		# Toggle between visible and captured mouse modes.
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
