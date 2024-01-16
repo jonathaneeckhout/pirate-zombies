e2eeeac20f2cdbe72f5ef471e8691492c356e1db
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-01-16 22:18:43 +0100

    handle animations

--- components/player/AnimationController/AnimationHandler.gd
+++ components/player/AnimationController/AnimationHandler.gd
@@ -2,13 +2,16 @@ extends Node
 
 class_name AnimationHandler
 
+# The AnimationPlayer node responsible for playing animations.
 @export var animation_player: AnimationPlayer
 
 # Reference to the parent node (assumed to be a Player node).
 var _player: Player = null
 
+# Flag indicating whether this instance represents the local player.
 var _is_own_player: bool = false
 
+# The previous position of the player.
 var _prev_position: Vector3 = Vector3.ZERO
 
 
@@ -20,35 +23,52 @@ func _ready():
 	# Ensure that the player's multiplayer connection is not null.
 	assert(_player.multiplayer_connection != null, "Player's multiplayer connection is null")
 
+	# If this node is on the server, disable physics processing, free the node, and return.
 	if _player.multiplayer_connection.is_server():
 		set_physics_process(false)
 		queue_free()
 		return
 
+	# Initialize the previous position with the current player position.
 	_prev_position = _player.position
 
+	# Check if this instance represents the local player.
 	_is_own_player = _player.multiplayer_connection.is_own_player(_player)
 
-	# Wait an additional frame so others can get set.
+	# Wait an additional frame to allow other nodes to be set up.
 	await get_tree().process_frame
 
 	# Some entities take a bit to get added to the tree, do not update them until then.
 	if not is_inside_tree():
 		await tree_entered
 
+	# Play the idle animation when the player enters the scene.
 	animation_player.play("idle")
 
 
+# Called in each physics frame to update the player's animations based on movement.
 func _physics_process(_delta):
+	# Calculate the movement vector between the current and previous player positions.
 	var movement: Vector3 = _player.position - _prev_position
+
+	# Transform the previous position into the local space of the player.
+	var reverse_direction: Vector3 = _player.to_local(_prev_position)
+
+	# Update the previous position to the current player position.
 	_prev_position = _player.position
 
+	# Check if the player is stationary.
 	if movement.is_zero_approx():
 		animation_player.play("idle")
 	else:
-		# if movement.x > 0:
-		# 	animation_player.play("run-right")
-		# elif movement.x < 0:
-		# 	animation_player.play("run-left")
-		# else:
-		animation_player.play("run-forward")
+		# Determine the animation based on the transformed movement vector.
+		if reverse_direction.x < -0.01:
+			animation_player.play("run-right")
+		elif reverse_direction.x > 0.01:
+			animation_player.play("run-left")
+		else:
+			# If the lateral movement is minimal, check the forward/backward movement.
+			if reverse_direction.z < -0.01:
+				animation_player.play("run-back")
+			else:
+				animation_player.play("run-forward")
--- scenes/Player/Model/Model.gd
+++ scenes/Player/Model/Model.gd
@@ -2,14 +2,14 @@ extends Node3D
 
 @export var head: Node3D = null
 
-
 @onready var skeleton: Skeleton3D = $Root/Skeleton3D
 
 var _chest_number: int = 0
+
+
 func _ready():
 	_chest_number = skeleton.find_bone("Chest")
-	print(_chest_number)
+
 
 func _physics_process(_delta):
 	skeleton.set_bone_pose_rotation(_chest_number, head.quaternion.inverse())
-
