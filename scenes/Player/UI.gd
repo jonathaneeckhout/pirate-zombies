91893271caf8ba0a36848af174afe2adbe5ada87
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-01-21 13:26:48 +0100

    show ui healthbar

---
+++ scenes/Player/UI.gd
@@ -0,0 +1,34 @@
+extends CanvasLayer
+
+@export var player: Player = null
+
+@export var stats_synchronizer: StatsSynchronizer = null
+
+@onready var health_bar: TextureProgressBar = $HealthBar
+
+const bar_green: Resource = preload("res://assets/healthbar/scaled/GreenBar.png")
+const bar_yellow: Resource = preload("res://assets/healthbar/scaled/YellowBar.png")
+const bar_red: Resource = preload("res://assets/healthbar/scaled/RedBar.png")
+
+
+func _ready():
+	if not player.multiplayer_connection.is_own_player(player):
+		queue_free()
+		return
+
+	update(stats_synchronizer.hp, stats_synchronizer.max_hp)
+
+	stats_synchronizer.hurt.connect(_on_hurt)
+
+
+func update(amount, full):
+	health_bar.texture_progress = bar_green
+	if amount < 0.75 * full:
+		health_bar.texture_progress = bar_yellow
+	if amount < 0.45 * full:
+		health_bar.texture_progress = bar_red
+	health_bar.value = amount
+
+
+func _on_hurt(_damage: int):
+	update(stats_synchronizer.hp, stats_synchronizer.max_hp)
